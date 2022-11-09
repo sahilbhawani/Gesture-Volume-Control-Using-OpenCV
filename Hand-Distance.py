8fab8f68a8f35c51df5521facf27bb6a3ebea027
sahilbhawani <80466452+sahilbhawani@users.noreply.github.com>
2022-11-10 00:30:40 +0530

    Add files via upload

---
+++ Hand-Distance.py
@@ -0,0 +1,58 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import math
+import numpy as np
+import cvzone
+
+# Webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# Hand Detector
+detector = HandDetector(detectionCon=0.8, maxHands=2)
+
+# Find Function
+# x is the raw distance y is the value in cm
+x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
+y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+coff = np.polyfit(x, y, 2)  # y = Ax^2 + Bx + C
+
+# Loop
+while True:
+    success, img = cap.read()
+    hands = detector.findHands(img, draw=False)
+    # print(len(hands))
+    print(hands)
+    closehand = []
+    if hands:
+
+        for i in range(len(hands)):
+
+            lmList = hands[i]['lmList']
+            x, y, w, h = hands[i]['bbox']
+            x1, y1 , z1 = lmList[5]
+            x2, y2 , z2 = lmList[17]
+
+            distance = int(math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2))
+            A, B, C = coff
+            distanceCM = A * distance ** 2 + B * distance + C
+            closehand.append(distanceCM)
+
+        # print(distanceCM, distance)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 3)
+
+    print(closehand)
+    x = len(closehand)
+    if(x==0):
+        print("No hands detected")
+    elif(x==1):
+        cvzone.putTextRect(img, f'{int(distanceCM)} cm', (50,100))
+    else:
+        if(closehand[0]>closehand[1]):
+            cvzone.putTextRect(img, f'{int(closehand[1])} cm', (50,100))
+        else:
+            cvzone.putTextRect(img, f'{int(closehand[0])} cm', (50,100))
+    # if(closehand)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
